788cb2e9ae13367d2eb2fa2f78ed712e4d01254e
Dima Lis <iselis777888999@gmail.com>
2023-10-19 11:16:27 +0300

    add yml-file

--- src/test/java/ru/netology/web/CardApplicationTest.java
+++ src/test/java/ru/netology/web/CardApplicationTest.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardApplicationTest {
